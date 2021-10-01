c2e4b483e9b5f5590d1214ff6755d6bd3d0e0aba
A5T <dev@a5tratto.it>
2021-10-01 18:35:04 +0200

    V.7.3.9.1 fix gmaps url

--- functions.php
+++ functions.php
@@ -264,7 +264,7 @@ function add_to_context_child($context)
     :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
     $gmaps_api_key = get_theme_mod('a5t_setting_maps');
-    $context['google_maps_api'] = 'http://maps.googleapis.com/maps/api/js?key=' . $gmaps_api_key . '&amp;sensor=false';
+    $context['google_maps_api'] = 'https://maps.googleapis.com/maps/api/js?key=' . $gmaps_api_key . '&amp;sensor=false&callback=initMap';
 
 
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
