9d89a7b73d85920d58d8f9a20afc2d1ae996871c
a5tratto-agency <dev@astratto.agency>
2023-01-25 21:48:36 +0100

    V.7.4.0.9

--- template-home-child.php
+++
@@ -1,9 +0,0 @@
-<!-- A_SETTINGS Impostazione pagina Template Home Child -->
-<?php
-/*
-Template Name: Template Home Child
-*/
-?>
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_part('templates/home'); ?>
-<?php endwhile; ?>
